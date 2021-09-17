7d3284e18d915dd3a417a10549fab21e7703573c
Brian Robinson <brob92993@gmail.com>
2021-09-17 13:10:09 -0400

    updated projects.json file with projects inside. started the layout for the home page and the projects page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./pages/About";
+import Homepage from "./pages/Homepage";
 import Projects from "./pages/Projects";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -13,7 +13,7 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={About} />
+          <Route exact path="/" component={Homepage} />
           <Route exact path="/projects" component={Projects} />
         </Wrapper>
         <Footer />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,39 @@
   
 import React from "react";
+import Projects from "../utils/projects.json"
+import Row from "../components/Row"
 
-function Projects() {
+
+function Project() {
   return (
+    <Row xs={1} md={2} className="g-4">
+      {Projects.map((project) => {
+        return (
+          <div key={project.id} className="card">
+            <img
+              src={project.image}
+              className="card-img-top"
+              alt={project.name + " screenshot"}
+            />
+            <div className="card-body">
+              <h4 className="card-title">
+                <a href="./images/nine.png" target="_blank" rel="noreferrer">
+                  {project.name}
+                </a>
+              </h4>
+              <h5 className="card-text">{project.description}</h5>
+              <h6>{project.tech}</h6>
+              <h6>
+                <a href={project.repo} target="_blank" rel="noreferrer">
+                  GitHub Repository
+                </a>
+              </h6>
+            </div>
+          </div>
+        );
+      })}
+    </Row>
+  );
+}
+
+export default Project;
